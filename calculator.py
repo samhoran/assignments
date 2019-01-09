a7bd8757fc3bf09bda7a2cede60b4a3fb5463c7e
samhoran <rhoran@jd20.law.harvard.edu>
2019-01-08 23:33:47 -0500

    add square, cube, and square n times functions

--- calculator.py
+++ calculator.py
@@ -10,3 +10,13 @@ def divide(a,b):
 print("I'm going to use the calculator functions to multiply 5 and 6")
 x = multiply(5,6)
 print(x)
+
+def square(a):
+	return a**2
+def cube(a):
+	return a**3
+def square_n_times(number, n):
+	a = number
+	for i in range(n):
+		a = a**2
+	return a

92de55c5d4a6c2b2b36972a1883071b886c87924
samhoran <rhoran@jd20.law.harvard.edu>
2019-01-15 16:12:32 -0500

    code up a vegetable csv writer

---
+++ pythondata/veggies.py
@@ -0,0 +1,16 @@
+import csv
+
+vegetables = [
+ {"name": "eggplant", "color": "purple"},
+ {"name": "tomato", "color": "red"},
+ {"name": "corn", "color": "yellow"},
+ {"name": "okra", "color": "green"},
+ {"name": "arugula", "color": "green"},
+ {"name": "broccoli", "color": "green"},
+]
+
+with open('veggies.csv', 'w') as f:
+	writer = csv.writer(f)
+	writer.writerow(['name', 'color', 'name length'])
+	for i in vegetables:
+		writer.writerow([i['name'], i['color'], len(i['name'])])

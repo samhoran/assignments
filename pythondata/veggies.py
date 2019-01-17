f10c314edad3994d8f8af916e93427ee1bc57324
samhoran <rhoran@jd20.law.harvard.edu>
2019-01-17 11:05:03 -0500

    go over veggies try it with class

--- pythondata/veggies.py
+++ pythondata/veggies.py
@@ -9,8 +9,24 @@ vegetables = [
  {"name": "broccoli", "color": "green"},
 ]
 
-with open('veggies.csv', 'w') as f:
-	writer = csv.writer(f)
-	writer.writerow(['name', 'color', 'name length'])
-	for i in vegetables:
-		writer.writerow([i['name'], i['color'], len(i['name'])])
+
+
+#write row to a CSV
+with open("vegetables.csv", "w") as f:
+    writer = csv.writer(f)
+    #Write header
+    writer.writerow(["name", "color","name_length"])
+    #Write data
+    #loop thru each vegetable
+    for vegetable in vegetables:
+        writer.writerow([vegetable["name"],vegetable["color"],len(vegetable["name"])])
+
+
+
+#I thought this was HW last night so I
+#did this, but I'll redo it in class with you now:
+#with open('veggies.csv', 'w') as f:
+#	writer = csv.writer(f)
+#	writer.writerow(['name', 'color', 'name length'])
+#	for i in vegetables:
+#		writer.writerow([i['name'], i['color'], len(i['name'])])

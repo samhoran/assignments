941b3a8dc92b3e14bad9d92fd09666cda4e1e220
samhoran <rhoran@jd20.law.harvard.edu>
2019-01-21 18:12:21 -0500

    note location of superhero try-it in Nurlan's repo

--- pythondata/powers.py
+++ pythondata/powers.py
@@ -1,2 +1,2 @@
 #Did this in pair programming with Nurlan, so it's in his repo
-#I just looked and it seems it's in the add-comments branch of the repo
+
--- pythondata/superheroes.py
+++ pythondata/superheroes.py
@@ -1,1 +1,4 @@
 #Did this in pair programming with Nurlan, so it's in his repo
+#I just looked and it seems it's in the add-comments branch of the repo as getmemberdata.py.
+#Sorry for any confusion.
+#Please let me have the points.
